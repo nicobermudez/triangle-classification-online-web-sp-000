1e01490971546c4fd74f0c3d82fe16d2ab0796bb
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-06 18:03:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,11 +10,7 @@ end
 
 # kind method should raise TriangleError
   def kind
-    if a<=0 || b<=0 || c<=0
-      raise TriangleError
-    elsif (a+b) <= c || (a+c) <= b || (b+c) <= a
-      raise TriangleError
-    else
+    validate triangle
       if a == b && b == c
         :equilateral
       elsif a == b || b == c || a == c
